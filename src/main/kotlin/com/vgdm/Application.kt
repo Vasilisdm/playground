233fb2d0724584bad5b23575596ac8e463e414d0
vasilisdmr <v.dimitriou@katanox.com>
2023-03-01 22:08:42 +0100

    Split configuration from routing

--- src/main/kotlin/com/vgdm/Application.kt
+++ src/main/kotlin/com/vgdm/Application.kt
@@ -1,5 +1,23 @@
 package com.vgdm
 
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
 fun main() {
-    println("hello world!")
+    embeddedServer(Netty, port = 4207, module = Application::module).start(wait = true)
+}
+
+fun Application.createKtorApplication() {
+    routing {
+        get("/") {
+            call.respondText("Hello, world!")
+        }
+    }
+}
+
+fun Application.module() {
+    createKtorApplication()
 }
