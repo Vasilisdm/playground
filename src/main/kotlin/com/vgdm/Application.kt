cf289d40adc0991676b10a560d4fcdded598e215
vasilisdmr <v.dimitriou@katanox.com>
2023-03-04 20:44:25 +0100

    Use regular expression to mask db's password

--- src/main/kotlin/com/vgdm/Application.kt
+++ src/main/kotlin/com/vgdm/Application.kt
@@ -10,6 +10,7 @@ import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.slf4j.LoggerFactory
+import kotlin.reflect.full.declaredMemberProperties
 
 private val logger = LoggerFactory.getLogger("com.vgdm.Application")
 
@@ -18,6 +19,21 @@ fun main() {
 
     val environment = System.getenv("KAPP_ENV") ?: "local"
     val config = appConfiguration(environment)
+
+    val secretsRegex = "password|secret|key"
+        .toRegex(RegexOption.IGNORE_CASE)
+
+    val configRepresentation = WebAppConfig::class.declaredMemberProperties
+        .sortedBy { it.name }
+        .map {
+            if (secretsRegex.containsMatchIn(it.name)) {
+                "${it.name} = ${it.get(config).toString().take(2)}*****"
+            } else {
+                "${it.name} = ${it.get(config)}"
+            } }
+        .joinToString(separator = "\n")
+
+    logger.debug("Current configuration: $configRepresentation")
 
     embeddedServer(Netty, port = config.httpPort, module = Application::module).start(wait = true)
 }
